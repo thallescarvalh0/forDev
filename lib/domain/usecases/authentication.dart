8c9ccca4258099ed36e74789b362c4c0b5ea1b06
thallescarvalh0 <thallescarvalhoferreira@gmail.com>
2021-09-20 21:13:59 -0300

    mover codigo de teste para producao de acordo com modelo

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,16 +3,14 @@ import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(
-      AuthenticationParams params);
+  Future<AccountEntity> auth(AuthenticationParams params);
 }
 
 class AuthenticationParams {
   final String email;
   final String secret;
 
-  AuthenticationParams({
-    @required this.email,
-    @required this.secret
-  });
+  AuthenticationParams({@required this.email, @required this.secret});
+
+  Map toJson() => {'email': email, 'password': secret};
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,29 +1,12 @@
 import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:forDev/domain/usecases/usecases.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
+import 'package:forDev/data/usecases/usecases.dart';
+import 'package:forDev/data/http/http.dart';
 
-  RemoteAuthentication({@required this.httpClient, @required this.url});
-
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email':params.email, 'password':params.secret};
-    await httpClient.request(url: url, method: 'post',
-    body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request(
-    {@required String url, 
-    @required String method,
-    Map body});
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
